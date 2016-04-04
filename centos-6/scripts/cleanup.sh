7d3aa84f68e088f8e69504f696e61ca0f84d082c
saitara <saito.yasuyuki@gmail.com>
2016-04-04 23:50:45 +0900

    ifcfg-lo対応(但し期待はずれ)

--- centos-6/scripts/cleanup.sh
+++ centos-6/scripts/cleanup.sh
@@ -5,11 +5,15 @@ rm -rf /tmp/rubygems-*
 
 # Inspired from http://blog.suz-lab.com/2013/01/amicentos6update.html
 yum clean all
+rm -rf /tmp/*.rpm
+rm -rf /tmp/rpsxps
 rm -f /etc/ssh/ssh_host_*
 rm -rf /var/lib/cloud
 cd /var/log
 ls -F /var/log | grep -v / | xargs -L1 cp /dev/null
 find /var/log/ -type f -name \*log | xargs -L1 cp /dev/null
+mv /etc/sysconfig/network-scripts/vmimport.ifcfg-lo /etc/sysconfig/network-scripts/ifcfg-lo
+rm /etc/sysconfig/network-scripts/ifcfg-eth0.vmimport
 cp /dev/null /var/log/audit/audit.log
 cp /dev/null /root/.ssh/authorized_keys
 cp /dev/null /root/.bash_history
