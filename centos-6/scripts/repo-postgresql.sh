d65cc24ea262b8ff74cb5cedb5d1ccea86550da7
saitara <saito.yasuyuki@gmail.com>
2016-04-04 19:13:16 +0900

    PostgreSQL 9.5対応

--- centos-6/scripts/repo-postgresql.sh
+++ centos-6/scripts/repo-postgresql.sh
@@ -1,1 +1,1 @@
-rpm -ivh http://yum.postgresql.org/9.4/redhat/rhel-6-x86_64/pgdg-centos94-9.4-1.noarch.rpm
+rpm -ivh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-6-x86_64/pgdg-centos95-9.5-2.noarch.rpm
