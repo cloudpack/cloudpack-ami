64e713b03688a51d60b3bde33dfbfc8dbd1e6961
saitara <saito.yasuyuki@gmail.com>
2016-04-07 17:50:44 +0900

    拡張repoはdisableにする

--- centos-6/scripts/repo-mysql.sh
+++ centos-6/scripts/repo-mysql.sh
@@ -1,5 +1,2 @@
 rpm -ivh http://dev.mysql.com/get/mysql57-community-release-el6-7.noarch.rpm
-sed -i.bak \
-  -e '/\[mysql56-community\]/,/gpgcheck=1/ s:enabled=.*:enabled=0:g' \
-  -e '/\[mysql57-community\]/,/gpgcheck=1/ s:enabled=.*:enabled=0:g' \
-  /etc/yum.repos.d/mysql-community.repo 
+sed -i -e 's:enabled=.*:enabled=0:g' /etc/yum.repos.d/mysql-community*.repo 
