3dfe67b106bf7d6eafbbbf08a8c7ad90fddc1c1a
saitara <saito.yasuyuki@gmail.com>
2016-04-18 17:47:26 +0900

    CentOS6と処理を揃える

--- centos-7/scripts/cleanup.sh
+++ centos-7/scripts/cleanup.sh
@@ -5,13 +5,13 @@ rm -rf /tmp/rubygems-*
 
 # Inspired from http://blog.suz-lab.com/2013/01/amicentos6update.html
 yum clean all
+rm -rf /tmp/*.rpm
+rm -rf /tmp/rpsxps
 rm -f /etc/ssh/ssh_host_*
 rm -rf /var/lib/cloud
 cd /var/log
 ls -F /var/log | grep -v / | xargs -L1 cp /dev/null
 find /var/log/ -type f -name \*log | xargs -L1 cp /dev/null
-cp /dev/null /var/log/audit/audit.log
-cp /dev/null /root/.ssh/authorized_keys
-cp /dev/null /root/.bash_history
-cp /dev/null /home/cloudpack/.ssh/authorized_keys
-cp /dev/null /home/cloudpack/.bash_history
+for toremove in /var/log/audit/audit.log /root/.ssh/authorized_keys /root/.bash_history /home/cloudpack/.ssh/authorized_keys /home/cloudpack/.bash_history;do
+	if [ -f ${toremove} ]; then cp /dev/null ${toremove};fi
+done
