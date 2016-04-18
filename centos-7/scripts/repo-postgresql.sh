f626b9a189ff28881fd3912b4fcb94ad22f451e8
saitara <saito.yasuyuki@gmail.com>
2016-04-18 17:45:33 +0900

    既定値として追加repoはenabled=0に変更

--- centos-7/scripts/repo-postgresql.sh
+++ centos-7/scripts/repo-postgresql.sh
@@ -1,1 +1,2 @@
-rpm -ivh http://yum.postgresql.org/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-1.noarch.rpm
+rpm -ivh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-2.noarch.rpm
+sed -i 's:enabled=.*:enabled=0:' /etc/yum.repos.d/pgdg-95-centos.repo
--- centos-7/scripts/repo-riak.sh
+++ centos-7/scripts/repo-riak.sh
@@ -1,6 +1,6 @@
 HOSTNAME=`hostname -f`
 FILENAME=/etc/yum.repos.d/basho.repo
 OS=el
-DIST=6
+DIST=7
 PACKAGE_CLOUD_RIAK_DIR=https://packagecloud.io/install/repositories/basho/riak
-curl "${PACKAGE_CLOUD_RIAK_DIR}/config_file.repo?os=${OS}&dist=${DIST}&name=${HOSTNAME}" > $FILENAME
+curl "${PACKAGE_CLOUD_RIAK_DIR}/config_file.repo?os=${OS}&dist=${DIST}&name=${HOSTNAME}" | sed 's:enabled=.*:enabled=0:' > $FILENAME
