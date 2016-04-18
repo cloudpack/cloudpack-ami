85e710d036f41571956a7a1e42783675780889ff
saitara <saito.yasuyuki@gmail.com>
2016-04-18 17:44:39 +0900

    Pub key取得部分追加調整

--- centos-6/scripts/vagrant.sh
+++ centos-6/scripts/vagrant.sh
@@ -1,6 +1,6 @@
 date > /etc/vagrant_box_build_time
 
 mkdir -pm 700 /home/cloudpack/.ssh
-curl -L https://raw.githubusercontent.com/mitchellh/cloudpack/master/keys/cloudpack.pub -o /home/cloudpack/.ssh/authorized_keys
+curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/cloudpack/.ssh/authorized_keys
 chmod 0600 /home/cloudpack/.ssh/authorized_keys
 chown -R cloudpack:cloudpack /home/cloudpack/.ssh
