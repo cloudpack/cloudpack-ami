fcba1203e97cfa87f4ff577f8a2f13a03af8c70a
saitara <saito.yasuyuki@gmail.com>
2017-01-16 21:08:12 +0900

    CentOS7の構築スクリプト構成変更

--- centos-7/scripts/base.sh
+++ centos-7/scripts/base.sh
@@ -8,7 +8,6 @@ yum -y install --enablerepo=epel \
 	cloud-utils \
 	cloud-utils-growpart \
 	lsof \
-	dkms \
 	python-pip
 pip install -U urllib3 pip
 pip install -U awscli
@@ -24,30 +23,4 @@ systemctl disable firewalld.service
 systemctl disable kdump.service
 systemctl disable sendmail.service
 
-# http://blog.father.gedow.net/2016/03/15/enhanced-networking/ を参照　多謝
-# ixgbevfソースのダウンロード（dkmsの都合で /usr/src である必要アリ）
-cd /usr/src
-curl -O -L https://sourceforge.net/projects/e1000/files/ixgbevf%20stable/3.3.2/ixgbevf-3.3.2.tar.gz
-tar xzf ixgbevf-3.3.2.tar.gz
-cd ixgbevf-3.3.2
- 
-# dkms用の設定
-cat <<'EOT' > dkms.conf
-PACKAGE_NAME="ixgbevf"
-PACKAGE_VERSION="3.3.2"
-CLEAN="cd src/; make clean"
-MAKE="cd src/; make BUILD_KERNEL=${kernelver}"
-BUILT_MODULE_LOCATION[0]="src/"
-BUILT_MODULE_NAME[0]="ixgbevf"
-DEST_MODULE_LOCATION[0]="/updates"
-DEST_MODULE_NAME[0]="ixgbevf"
-AUTOINSTALL="yes"
-EOT
-# インストール
-dkms add     -m ixgbevf -v 3.3.2
-dkms build   -m ixgbevf -v 3.3.2
-dkms install -m ixgbevf -v 3.3.2
- 
-# モジュールが更新されたのを確認
-modinfo ixgbevf
 rm -rf /tmp/*
--- centos-7/scripts/cloudpack.sh
+++ centos-7/scripts/cloudpack.sh
@@ -58,11 +58,6 @@ ls vmimport.ifcfg-* && rm vmimport.ifcfg-*
 cd /etc/udev/rules.d
 ls *vmimport && rm *vmimport
 
-#dkms remove -m ixgbevf/3.3.2 --all
-#dkms add -m ixgbevf -v 3.3.2
-#dkms build -m ixgbevf -v 3.3.2
-#dkms install -m ixgbevf -v 3.3.2
-
 cat << EOT >> /etc/sysconfig/network
 IPV6INIT=no
 DHCPV6C=no
@@ -104,29 +99,4 @@ EOT
 cat << EOT >> /etc/security/limits.d/99-cloudpack.conf
 * soft nofile 65536
 * hard nofile 65536
-EOT
-
-cat << EOT >> /etc/profile.d/motd.sh
-CURL_CMD="curl --max-time 2 --connect-timeout 2 -s"
-echo "####"
-echo -e "#### You have logged in to \e[1mlocalhost.localdomain\e[m as \e[1m\$(id -n -u)\e[m successfully."
-InstanceID=\$( \${CURL_CMD} 169.254.169.254/latest/meta-data/instance-id)
-if [ \$? -eq 0 ]; then
-	echo -e "#### This server is running on \e[1m\e[33mAWS\e[m."
-	echo "####   Instance ID:       \$( \${CURL_CMD} 169.254.169.254/latest/meta-data/instance-id)"
-	echo "####   Instance Type:     \$( \${CURL_CMD} 169.254.169.254/latest/meta-data/instance-type)"
-	echo "####   Availability Zone: \$( \${CURL_CMD} 169.254.169.254/latest/meta-data/placement/availability-zone)"
-	echo "####   Private IP:        \$( \${CURL_CMD} 169.254.169.254/latest/meta-data/local-ipv4)"
-	public_ip=\$( \${CURL_CMD} 169.254.169.254/latest/meta-data/public-ipv4 | head -n 1 | grep -e "^[^<]")
-	echo "####   Public IP:         \$public_ip"
-else
-	echo "####   Private IP:        \$(/sbin/ip -f inet addr show | gawk '\$0 ~ /inet/ {print \$2}'| grep -v "127.0.0.1")"
-fi
-echo "####"
-EOT
-
-cat << EOT >> /etc/profile.d/bash_completion.sh
-# history にコマンド実行時刻を記録する
-HISTTIMEFORMAT='%Y-%m-%dT%T%z '
-HISTSIZE=1000000
 EOT
