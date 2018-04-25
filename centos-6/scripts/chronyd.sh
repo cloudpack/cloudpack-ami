1c66710e2a33c84e6b93a11efcb33eae4ea7e8b2
saitara <saito.yasuyuki@gmail.com>
2018-04-25 20:38:41 +0900

    時刻同期をAWS内で完結する

--- centos-6/scripts/chronyd.sh
+++ centos-6/scripts/chronyd.sh
@@ -1,7 +1,9 @@
 yum install -y --enablerepo=epel chrony
 chkconfig chronyd on
+CONF=/etc/chrony.conf
 
-echo "leapsecmode slew" >> /etc/chrony.conf
-echo "maxslewrate 1000" >> /etc/chrony.conf
-echo "smoothtime 400 0.001 leaponly" >> /etc/chrony.conf
-
+echo "leapsecmode slew" >> ${CONF}
+echo "maxslewrate 1000" >> ${CONF}
+echo "smoothtime 400 0.001 leaponly" >> ${CONF}
+sed -e '/server /D' ${CONF}
+echo "server 169.254.169.123 prefer iburst" >> ${CONF}
